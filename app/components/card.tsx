07dc4a8f7e9d5c604d644e7b2ba33bf421fdf72c
Mohammed <mtajir903@gmail.com>
2026-01-30 19:25:25 +0530

    page 3 card 2 done

---
+++ app/components/card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cn } from "../lib/utils/cn";
+export const Card = ({
+  mainComponent,
+  title,
+  paragraph,
+  className,
+}: {
+  mainComponent: React.ReactNode;
+  title: string;
+  paragraph: string;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn([
+        "z-0 flex flex-col rounded-2xl border-2 border-neutral-200 bg-white",
+        className,
+      ])}
+    >
+      {mainComponent}
+      <div className="px-8 pb-6">
+        <h1 className="font-inter text-2xl font-semibold">{title}</h1>
+        <p className="tracking-tighter text-neutral-500">{paragraph}</p>
+      </div>
+    </div>
+  );
+};
--- app/pages/page3.tsx
+++ app/pages/page3.tsx
@@ -1,71 +1,201 @@
 "use client";
-import Image from 'next/image'
-import { animate, easeIn, motion, useMotionValue, useTransform } from "motion/react"
-import { useEffect } from 'react';
+import Image from "next/image";
+import {
+  animate,
+  motion,
+  useAnimate,
+  useMotionValue,
+  useMotionValueEvent,
+  useTransform,
+} from "motion/react";
+import { useEffect } from "react";
+import { Card } from "@/app/components/card";
+import { Clock } from "../icons/clock";
 
 export const Page3 = () => {
   const t = useMotionValue(0);
-  
+  const [scope, animate1] = useAnimate();
+  const CardTwo1stRotation = async () => {
+    await animate1(
+      ".component1",
+      { scale: [1, 1.3], opacity: 0 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component2",
+      { scale: 1.2, zIndex: 10, y: -20 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component3",
+      { scale: 1.2, zIndex: 5, y: -25 },
+      { duration: 0.3 },
+    );
+
+    await animate1(
+      ".component1",
+      { scale: 0.8, y: 30, zIndex: 0 },
+      { duration: 0.3 },
+    );
+
+    await animate1(".component1", { opacity: 1 }, { duration: 0.3 });
+  };
+
+  const CardTwo2ndRotation = async () => {
+    await animate1(
+      ".component2",
+      { scale: [1, 1.2], y: -25, opacity: 0 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component3",
+      { scale: 1.3, zIndex: 10, y: -35 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component1",
+      { scale: 0.9, zIndex: 5, y: 25 },
+      { duration: 0.3 },
+    );
+
+    await animate1(
+      ".component2",
+      { scale: 0.85, y: 25, zIndex: 0 },
+      { duration: 0.3 },
+    );
+
+    await animate1(".component2", { opacity: 1 }, { duration: 0.2 });
+  };
+
+  const CardTwo3rdRotation = async () => {
+    await animate1(
+      ".component3",
+      { scale: [1, 1.2], y: -25, opacity: 0 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component1",
+      { scale: 1, zIndex: 10, y: 0 },
+      { duration: 0.3 },
+    );
+    await animate1(
+      ".component2",
+      { scale: 1, zIndex: 5, y: 0 },
+      { duration: 0.3 },
+    );
+
+    await animate1(
+      ".component3",
+      { scale: 1, y: 0, zIndex: 0 },
+      { duration: 0.3 },
+    );
+
+    await animate1(".component3", { opacity: 1 }, { duration: 0.2 });
+  };
+
+  const Card2Animations = async () => {
+    // div 1
+    await CardTwo1stRotation();
+
+    // div 2
+    await CardTwo2ndRotation();
+
+    // 3rd div
+    await CardTwo3rdRotation();
+  };
   useEffect(() => {
-    const controls = animate(t , 1 , {
-      duration : 5,
-      repeat : Infinity,
-      ease : "linear"
-    })
-    return () => controls.stop()
-  }, [])
+    const controls = animate(t, 1, {
+      duration: 5,
+      repeat: Infinity,
+      ease: "linear",
+    });
+    Card2Animations();
+    return () => controls.stop();
+  }, []);
+
   return (
-    <div className='min-h-screen mt-15'>
-        <div>
-            <h1 className='text-7xl mx-auto font-inter tracking-tighter font-semibold w-fit'>
-                Built for everyone
-            </h1>
-            <p className='text-neutral-400 text-center max-w-sm mx-auto mt-3 tracking-tight leading-5'>
-                Thousand of business, from startups to enterprises, use
-                CoreShift to handle payments.
-            </p>
-        </div>
-        <div className='grid grid-cols-3 grid-rows-2 mx-auto px-30 gap-2 mt-10'>
-            
-            <div className='bg-white flex flex-col border-2 z-0 rounded-2xl border-neutral-200'>
-                <div className=' flex  h-full w-full items-center justify-around gap-4 overflow-hidden py-10'>
-                    <div className='bg-linear-to-b from-neutral-200 via-neutral-300 to-neutral-200 h-full rounded-r-2xl w-full max-w-20'></div>
-                    <div className=''>
-                        
-                    <Image src={"/images/p3topleftPic.png"} className='h-35 shadow-xl' alt={"someImage"} height={500} width={500} />
-                    </div>
-                    <div className='bg-linear-to-b from-neutral-200 via-neutral-300 to-neutral-200 h-full rounded-l-2xl w-full max-w-20'></div>
-                </div>
-                <div className='px-8 pb-6'>
-                    <h1 className='font-semibold text-2xl  font-inter'>For HR professionals</h1>
-                    <p className='text-neutral-500 tracking-tighter'>
-                        Use a single cloud system for your empoloyee.
-                        candidate and HR processes info.
-                    </p>
-                </div>
+    <div ref={scope} className="mt-15 min-h-screen">
+      <div>
+        <h1 className="mx-auto w-fit font-inter text-7xl font-semibold tracking-tighter">
+          Built for everyone
+        </h1>
+        <p className="mx-auto mt-3 max-w-sm text-center leading-5 tracking-tight text-neutral-400">
+          Thousand of business, from startups to enterprises, use CoreShift to
+          handle payments.
+        </p>
+      </div>
+      <div className="mx-auto mt-10 grid grid-cols-3 grid-rows-2 gap-2 px-30">
+        <Card
+          mainComponent={
+            <div className="flex h-full w-full items-center justify-around gap-4 overflow-hidden py-10">
+              <div className="h-full w-full max-w-20 rounded-r-2xl bg-linear-to-b from-neutral-200 via-neutral-300 to-neutral-200"></div>
+              <div className="">
+                <Image
+                  src={"/images/p3topleftPic.png"}
+                  className="h-35 shadow-xl"
+                  alt={"someImage"}
+                  height={500}
+                  width={500}
+                />
+              </div>
+              <div className="h-full w-full max-w-20 rounded-l-2xl bg-linear-to-b from-neutral-200 via-neutral-300 to-neutral-200"></div>
             </div>
-            <div className='border-2 rounded-2xl border-neutral-200 z-10 overflow-hidden flex flex-col bg-white relative'>
-                <div className='mask-x-from-10% h-full z-0 absolute inset-0'>
-                    {Array.from({ length : 4 }).map((_,index) => {
-                    const phase = index * 0.25 
-                    const scale = useTransform(t , v => (v + phase) %1)
-                    
-                    return (<motion.div
-                    style={{
+          }
+          title={"For HR professionals"}
+          paragraph="Use a single cloud system for your empoloyee candidate and HR processes info."
+        />
+        <Card
+          className="relative"
+          mainComponent={
+            <div className="h-full">
+              <div className="absolute inset-0 z-0 h-full mask-x-from-10%">
+                {Array.from({ length: 4 }).map((_, index) => {
+                  const phase = index * 0.25;
+                  const scale = useTransform(t, (v) => (v + phase) % 1);
+
+                  return (
+                    <motion.div
+                      style={{
                         scale,
-                    }}
-                    key={index}
-                    className='mx-auto border-2 w-70 h-70 border-neutral-400 rounded-full absolute aspect-square inset-x-0 -translate-y-10 z-10'>
-                    </motion.div>)})}
-                </div>
-                <div className='flex relative justify-center z-40 items-center h-full w-full'>
-                    hi there
-                </div>
-                <div className=' relative z-30 bg-white w-full h-40'>
-                    
-                </div>
+                      }}
+                      key={index}
+                      className="absolute inset-x-0 z-10 mx-auto aspect-square h-65 -translate-y-5 rounded-full border-2 border-neutral-400"
+                    ></motion.div>
+                  );
+                })}
+              </div>
+              <div className="relative z-40 flex h-full w-full flex-col items-center justify-center">
+                <motion.div className="flex h-fit flex-col">
+                  <motion.div
+                    style={{}}
+                    className="component1 z-10 flex items-center gap-3 rounded-lg border border-neutral-200 bg-white p-2 font-medium tracking-tight shadow-p3c2container"
+                  >
+                    <motion.div className="rounded-xl bg-linear-to-br from-[#5ed7ff] via-[#16baf0] to-[#30beee] p-2.5 text-white shadow-p3c2subcontainer">
+                      <Clock />
+                    </motion.div>
+                    Access Real-Time Insights
+                  </motion.div>
+                  <div className="component2 z-5 -mt-8 flex scale-90 items-center gap-3 rounded-lg border border-neutral-200 bg-white p-2 shadow-p3c2container">
+                    <div className="rounded-md bg-blue-400 p-1.5 text-white">
+                      <Clock />
+                    </div>
+                    Access Real-Time Insights
+                  </div>
+                  <div className="component3 -mt-8 flex scale-80 items-center gap-3 rounded-lg border border-neutral-200 bg-white p-2 shadow-p3c2container">
+                    <div className="rounded-md bg-blue-400 p-1.5 text-white">
+                      <Clock />
+                    </div>
+                    Access Real-Time Insights
+                  </div>
+                </motion.div>
+              </div>
             </div>
-        </div>
+          }
+          title="For managers & leaders"
+          paragraph="Get always up-to-date data and monitor performance of the company."
+        />
+        <div className="h-full w-full rounded-2xl border-2 border-neutral-200 bg-white"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
